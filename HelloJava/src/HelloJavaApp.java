7fa63343f1021da67b663d46a534f2c845eec2e6
dejan-marlovic <dmarlovic83@gmail.com>
2024-12-23 14:23:14 +0100

    formatted code

--- HelloJava/src/HelloJavaApp.java
+++ HelloJava/src/HelloJavaApp.java
@@ -2,5 +2,6 @@ public class HelloJavaApp {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
         System.out.println("Rob was here");
+        System.out.println("Dejan was here");
     }
 }
